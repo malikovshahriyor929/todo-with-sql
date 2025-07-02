b23bc3432cddd51a252eae6c5040fd609633f062
malikovshahriyor <malikovshahriyor929@gmail.com>
2025-07-02 12:37:28 +0500

    chat

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaClient } from "@prisma/client";
 
 import postService from "../services/post.service.js";
 
